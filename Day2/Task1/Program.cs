00249a70b795521ae9d08ab2c3e109b03f38015a
JeyDim <sergeynovik73@gmail.com>
2021-12-03 00:03:44 +0400

    [day 2]

---
+++ Day2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿(int, int) AggregateFunc((int horizontalPos, int depth) tuple, string currentCommand)
+{
+    var split = currentCommand.Split(' ');
+    var command = split[0];
+    var value = int.Parse(split[1]);
+    switch (command)
+    {
+        case "forward":
+            tuple.horizontalPos += value;
+            break;
+        case "down":
+            tuple.depth += value;
+            break;
+        case "up":
+            tuple.depth -= value;
+            break;
+    }
+    return tuple;
+}
+
+var (horizontalPos, depth) = CommonFunctions.ReadFunctions.ReadAllData()
+    .TakeWhile(s => !s.Equals(string.Empty))
+    .Aggregate(
+    (horisontalPos: 0, depth: 0),
+    AggregateFunc);
+
+Console.WriteLine(horizontalPos * depth);
---
+++ Day2/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿(int, int, int) AggregateFunc((int horizontalPos, int depth, int aim) tuple, string currentCommand)
+{
+    var split = currentCommand.Split(' ');
+    var command = split[0];
+    var value = int.Parse(split[1]);
+    switch (command)
+    {
+        case "forward":
+            tuple.horizontalPos += value;
+            tuple.depth += value * tuple.aim;
+            break;
+        case "down":
+            tuple.aim += value;
+            break;
+        case "up":
+            tuple.aim -= value;
+            break;
+    }
+    return tuple;
+}
+
+var (horizontalPos, depth, aim) = CommonFunctions.ReadFunctions.ReadAllData()
+    .TakeWhile(s => !s.Equals(string.Empty))
+    .Aggregate(
+    (horisontalPos: 0, depth: 0, aim: 0),
+    AggregateFunc);
+
+Console.WriteLine(horizontalPos * depth);
