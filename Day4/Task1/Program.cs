62ddcb8a9d289bc77a46b6db304dc08cd8794f29
JeyDim <sergeynovik73@gmail.com>
2021-12-06 22:16:54 +0400

    [day 4]

---
+++ Day4/Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿using static CommonFunctions.Helpers;
+
+var winNumbers = Console.ReadLine()!.Split(',').ToList();
+
+List<List<string[]>> boards = new();
+var endOfContent = false;
+do
+{
+    var data = ReadAllData().ToList();
+    if (data.Count > 0)
+    {
+        boards.Add(data.Select(x => x.Split(' ').Where(s => !string.IsNullOrEmpty(s)).ToArray()).ToList());
+    }
+    else
+    {
+        if (endOfContent)
+            break;
+
+        endOfContent = true;
+    }
+} while (true);
+
+foreach (var number in winNumbers)
+{
+    foreach (var board in boards)
+    {
+        foreach (var row in board)
+        {
+            for (var i = 0; i < row.Length; i++)
+            {
+                if (row[i].Equals(number))
+                    row[i] = "z";
+            }
+        }
+        if (CheckBoard(board))
+        {
+            var sum = board.Select(row => row.Where(x => !x.Equals("z"))
+                    .Select(x => Convert.ToInt32(x))
+                    .ToList())
+                .Select(numbers => numbers.Sum())
+                .Sum();
+            Console.WriteLine(sum * Convert.ToInt32(number));
+            return;
+        }
+    }
+}
+
+bool CheckBoard(IReadOnlyCollection<string[]> board)
+{
+    if (board.Any(row => row.All(c => c == "z")))
+    {
+        return true;
+    }
+    for (var i = 0; i < board.Count; i++)
+    {
+        if (board.Select(row => row[i]).All(c => c == "z"))
+            return true;
+    }
+    return false;
+}
---
+++ Day4/Task2/Program.cs
@@ -0,0 +1,72 @@
+﻿using static CommonFunctions.Helpers;
+
+var winNumbers = Console.ReadLine()!.Split(',').ToList();
+
+Dictionary<int, List<string[]>> boards = new();
+var endOfContent = false;
+var id = 0;
+do
+{
+    var data = ReadAllData().ToList();
+    if (data.Count > 0)
+    {
+        boards.Add(id++, data.Select(x => x.Split(' ').Where(s => !string.IsNullOrEmpty(s)).ToArray()).ToList());
+    }
+    else
+    {
+        if (endOfContent)
+            break;
+
+        endOfContent = true;
+    }
+} while (true);
+
+var winners = new List<int>();
+foreach (var number in winNumbers)
+{
+    foreach (var board in boards)
+    {
+        foreach (var row in board.Value)
+        {
+            for (var i = 0; i < row.Length; i++)
+            {
+                if (row[i].Equals(number))
+                    row[i] = "z";
+            }
+        }
+        if (CheckBoard(board.Value))
+        {
+            if (winners.Contains(board.Key))
+                continue;
+
+            if (winners.Count < boards.Count - 1)
+            {
+                winners.Add(board.Key);
+            }
+            else
+            {
+                var sum = board.Value.Select(row => row.Where(x => !x.Equals("z"))
+                        .Select(x => Convert.ToInt32(x))
+                        .ToList())
+                    .Select(numbers => numbers.Sum())
+                    .Sum();
+                Console.WriteLine(sum * Convert.ToInt32(number));
+                return;
+            }
+        }
+    }
+}
+
+bool CheckBoard(IReadOnlyCollection<string[]> board)
+{
+    if (board.Any(row => row.All(c => c == "z")))
+    {
+        return true;
+    }
+    for (var i = 0; i < board.Count; i++)
+    {
+        if (board.Select(row => row[i]).All(c => c == "z"))
+            return true;
+    }
+    return false;
+}
