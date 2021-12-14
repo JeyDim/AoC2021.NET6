c48e62994af0006b4f4189c46ae1e5a309e99705
JeyDim <sergeynovik73@gmail.com>
2021-12-14 16:09:21 +0400

    [day 5]

---
+++ Day5/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using static CommonFunctions.Helpers;
+
+var data = ReadAllData()
+    .Select(s => s.Replace(" -> ", ",").Split(','))
+    .Select(s => s.Select(d => Convert.ToInt32(d)).ToList())
+    .ToList();
+
+var map = new int [1000, 1000];
+
+
+foreach (var vent in data)
+{
+    int min, max;
+    if (vent[1] == vent[3] && vent[0] != vent[2])
+    {
+        min = Math.Min(vent[0], vent[2]);
+        max = Math.Max(vent[0], vent[2]);
+        for (var i = min; i <= max; i++)
+        {
+            map[vent[1], i]++;
+        }
+    }
+
+    if (vent[0] == vent[2] && vent[1] != vent[3])
+    {
+        min = Math.Min(vent[1], vent[3]);
+        max = Math.Max(vent[1], vent[3]);
+        for (var i = min; i <= max; i++)
+        {
+            map[i, vent[0]]++;
+        }
+    }
+}
+
+var sum = map.Cast<int>().Count(value => value > 1);
+
+Console.WriteLine(sum);
---
+++ Day5/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using static CommonFunctions.Helpers;
+
+var data = ReadAllData()
+    .Select(s => s.Replace(" -> ", ",").Split(','))
+    .Select(s => (x1: int.Parse(s[0]), y1: int.Parse(s[1]), x2: int.Parse(s[2]), y2: int.Parse(s[3])))
+    .ToList();
+
+var map = new int [1000, 1000];
+
+
+foreach (var vent in data)
+{
+    var dx = Math.Sign(vent.x2 - vent.x1);
+    var dy = Math.Sign(vent.y2 - vent.y1);
+
+    var x = vent.x1;
+    var y = vent.y1;
+    while (x != vent.x2 + dx || y != vent.y2 + dy)
+    {
+        map[x, y]++;
+        x += dx;
+        y += dy;
+    }
+}
+
+var sum = map.Cast<int>().Count(value => value > 1);
+
+Console.WriteLine(sum);
